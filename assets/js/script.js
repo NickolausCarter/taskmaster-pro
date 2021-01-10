61802bbbd4a8e4d1e660aff5dccf32d4b13af2c8
Pure KaoZ <nickolaus.carter81@gmail.com>
2021-01-10 02:10:26 -0600

    ui/ux improvements

--- assets/js/script.js
+++ assets/js/script.js
@@ -155,33 +155,35 @@ $(".card .list-group").sortable({
   scroll: false,
   tolerance: "pointer",
   helper: "clone",
-  // activate: function(event) {
-  //   console.log("activate", this);
-  // },
-  // deactivate: function(event) {
-  //   console.log("deactivate", this);
-  // },
-  // over: function(event) {
-  //   console.log("over", event.target);
-  // },
-  // out: function(event) {
-  //   console.log("out", event.target);
-  // },
+  activate: function(event) {
+    $(this).addClass("dropover");
+    $(".bottom-trash").addClass("bottom-trash-drag");
+  },
+  deactivate: function(event) {
+    $(this).removeClass("dropover");
+    $(".bottom-trash").removeClass("bottom-trash-drag");
+  },
+  over: function(event) {
+    $(event.target).addClass("dropover-active");
+  },
+  out: function(event) {
+    $(event.target).removeClass("dropover-active");
+  },
   update: function(event) {
     var tempArr = [];
-
+    
     // loop through list-group to arrange list items
     $(this).children().each(function() {
       var text = $(this)
-        .find("p")
-        .text()
-        .trim();
-
+      .find("p")
+      .text()
+      .trim();
+      
       var date = $(this)
-        .find("span")
-        .text()
-        .trim();
-
+      .find("span")
+      .text()
+      .trim();
+      
       tempArr.push({
         text: text,
         date: date
@@ -190,9 +192,9 @@ $(".card .list-group").sortable({
     
     // save updated list to tasks
     var arrName = $(this)
-      .attr("id")
-      .replace("list-", "");
-
+    .attr("id")
+    .replace("list-", "");
+    
     tasks[arrName] = tempArr;
     saveTasks();
   }
@@ -203,13 +205,13 @@ $("#trash").droppable({
   tolerance: "touch",
   drop: function(event, ui) {
     ui.draggable.remove();
-    console.log("drop");
+    $(".bottom-trash").removeClass("bottom-trash-active");
   },
   over: function(event, ui) {
-    console.log("over");
+    $(".bottom-trash").addClass("bottom-trash-active");
   },
   out: function(event, ui) {
-    console.log("out");
+    $(".bottom-trash").removeClass("bottom-trash-active");
   }
 });
 
@@ -230,7 +232,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -278,7 +280,11 @@ var auditTask = function(taskEl) {
     $(taskEl).addClass("list-group-item-warning");
   }
 };
-
+setInterval(function() {
+  $(".card .list-group-item").each(function(index, el) {
+    auditTask(el);
+  });
+}, (1000 * 60) * 30);
 // load tasks for the first time
 loadTasks();
 
